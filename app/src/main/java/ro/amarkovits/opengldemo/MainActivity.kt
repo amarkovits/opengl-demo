4ffa5e229e5dd163c1d1b315afd6bd9abd387146
Andrei Markovits <andrei@Andreis-MacBook-Pro.local>
2018-07-23 17:03:12 +0300

    Initial commit

---
+++ app/src/main/java/ro/amarkovits/opengldemo/MainActivity.kt
@@ -0,0 +1,39 @@
+package ro.amarkovits.opengldemo
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.opengl.GLSurfaceView
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var mGLView: GLSurfaceView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mGLView = MyGLSurfaceView(this)
+        setContentView(mGLView)
+
+    }
+}
+
+internal class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
+
+    private val mRenderer: MyGLRenderer
+
+    init {
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2)
+
+        mRenderer = MyGLRenderer()
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(mRenderer)
+
+        renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+    }
+}
